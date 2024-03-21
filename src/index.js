e8722c10011bd56e902defb2218bba2153f6b215
PengPeng <pengpeng@fun.tv>
2024-03-21 15:27:02 +0800

    add local webpack compiler

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ function component() {
   const element = document.createElement('div');
   const [a, b] = [10 , 20];
   const result = add(a, b);
-  element.innerHTML = `Hello webpack in bundle, ${a} + ${b} = ${result}`;
+  element.innerHTML = `Hello webpack in bundle by local compiler, ${a} + ${b} = ${result}`;
   return element;
 }
 
