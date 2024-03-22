88e84144de8eee0ca6ec0fe5a6bb1f186755ab4f
PengPeng <pengpeng@fun.tv>
2024-03-22 12:49:16 +0800

    use middle ware serve for watch

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,25 @@
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: {
     index: './src/index.js',
     add: './src/add.js',
   },
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: '管理输出',
+    }),
+  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true
+    clean: true,
+    publicPath: '/',
   },
   mode: 'development',
 };
